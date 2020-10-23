74cfcef23ddd8d1001cfe72124dedf383be5b6a9
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2020-10-23 08:14:44 +0200

    WIP

--- src/ControllerRegistry.ts
+++ src/ControllerRegistry.ts
@@ -2,3 +2,4 @@ import './OAuth/Infraestructure/HomeController';
 import './OAuth/Infraestructure/UserController';
 import './OAuth/Infraestructure/GoogleController';
 import './OAuth/Infraestructure/SlackController';
+import './OAuth/Infraestructure/GithubController';
--- src/OAuth/Infraestructure/PassportConfig.js
+++ src/OAuth/Infraestructure/PassportConfig.js
@@ -4,7 +4,7 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const SlackStrategy = require('passport-slack-oauth2').Strategy;
 const GitHubStrategy = require('passport-github2').Strategy;
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
   /*
       From the user take just the id (to minimize the cookie size) and just pass the id of the user
       to the done callback
@@ -13,7 +13,7 @@ passport.serializeUser(function(user, done) {
   done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
+passport.deserializeUser(function (user, done) {
   /*
       Instead of user this function usually recives the id
       then you use the id to select the user from the db and pass the user obj to the done callback
@@ -29,7 +29,7 @@ passport.use(
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       passReqToCallback: true,
     },
-    function(request, accessToken, refreshToken, profile, done) {
+    function (request, accessToken, refreshToken, profile, done) {
       /*
      use the profile info (mainly profile id) to check if the user is registerd in ur db
      If yes select the user and pass him to the done callback
@@ -47,7 +47,7 @@ passport.use(
       clientSecret: process.env.SLACK_CLIENT_SECRET,
       skipUserProfile: false,
     },
-    function(accessToken, refreshToken, profile, done) {
+    function (accessToken, refreshToken, profile, done) {
       return done(null, profile);
     },
   ),
@@ -60,7 +60,7 @@ passport.use(
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       skipUserProfile: false,
     },
-    function(accessToken, refreshToken, profile, done) {
+    function (accessToken, refreshToken, profile, done) {
       return done(null, profile);
     },
   ),
