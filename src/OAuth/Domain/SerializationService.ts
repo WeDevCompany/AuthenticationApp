a37424d654845c0419e8c9e2c47e286cd3e8feb7
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2020-10-24 13:03:01 +0200

    WIP

---
+++ src/OAuth/Domain/SerializationService.ts
@@ -0,0 +1,4 @@
+export interface SerializationService {
+  serializeUser();
+  deserializeUser();
+}
---
+++ src/OAuth/Infraestructure/PassportSerializationService.ts
@@ -0,0 +1,6 @@
+import { SerializationService } from '../Domain/SerializationService';
+
+class PassportSerializationService implements SerializationService {
+  public serializeUser() {}
+  public deserializeUser() {}
+}
