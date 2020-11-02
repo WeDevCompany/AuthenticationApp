c0929b3ca46eab12f6c10d8d83d2d2f1dde53370
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2020-11-03 00:39:31 +0100

    WIP :fire:

---
+++ src/__test__/OAuth/Application/CreateGoogleUser.test.ts
@@ -0,0 +1,43 @@
+import { ConsoleLogger } from '../../../OAuth/Infraestructure/ConsoleLogger';
+import { FakeUserRepository } from '../Infraestructure/FakeUserRepository';
+import { UserRepository } from '../../../OAuth/Domain/UserRepository';
+import { ValidUser } from '../../../OAuth/Domain/ValidUser';
+import { User } from '../../../OAuth/Domain/User';
+
+const USERS_DATA: ValidUser[] = [
+  {
+    id: '1dfgsdfg',
+    displayName: 'Della',
+    username: 'Cox',
+    image: 'https://goole.com',
+    email: 'DellaDCox@superrito.com',
+  },
+  {
+    id: 'sdfsdf',
+    displayName: 'Denn',
+    username: 'dsfgff',
+    image: 'https://twitter.com',
+    email: 'Denn@dsfgff.com',
+  },
+];
+describe('It should validate the CreateGoogleUser application service', () => {
+  let repository: UserRepository;
+  beforeAll(() => {
+    const logger = new ConsoleLogger();
+    repository = new FakeUserRepository(logger);
+  });
+
+  it('should getAll data from the repository', async () => {
+    USERS_DATA.forEach(async user => {
+      expect(await repository.createUser(new User(user))).toBe(true);
+    });
+  });
+
+  /*it('should create a new user with valid data', async () => {
+        const spy = jest.spyOn(FakeWrite, 'write');
+        USERS_DATA.forEach(user => {
+            repository.create(user);
+            expect(spy).toHaveBeenCalled();
+        });
+    });*/
+});
---
+++ src/__test__/OAuth/Infraestructure/FakeUserRepository.ts
@@ -0,0 +1,21 @@
+import { Logger } from '../../../Logger';
+import { User } from '../../../OAuth/Domain/User';
+import { UserRepository } from '../../../OAuth/Domain/UserRepository';
+
+class FakeUserRepository implements UserRepository {
+  readonly logger: Logger;
+
+  constructor(logger: Logger) {
+    this.logger = logger;
+  }
+
+  async createUser(user: User) {
+    return true;
+  }
+
+  async deleteUser(id: string) {
+    return true;
+  }
+}
+
+export { FakeUserRepository };
