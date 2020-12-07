fa5dd3f19e45db32c70c92edaf4c7ed4124b177c
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2020-12-07 11:25:26 +0100

    WIP fixinig the database generation script, the cli to run typeorm commands

--- scripts/typeorm/typeorm-show.sh
+++ scripts/typeorm/typeorm-show.sh
@@ -4,4 +4,4 @@ CONTAINER_ID=$(grep -w CONTAINER_SERVICE_NAME .env | cut -d '=' -f2)
 
 echo "☢ Executing TypeORM command ${@} with N args ${#}"
 echo "============================"
-docker-compose exec $CONTAINER_ID ./node_modules/.bin/ts-node ./node_modules/.bin/typeorm -f ormconfig.cli.ts $@
+docker-compose exec $CONTAINER_ID npm run typeorm:show ${@} 
---
+++ scripts/typeorm/typeorm-suscriber.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Getting .env values to use on the script
+CONTAINER_ID=$(grep -w CONTAINER_SERVICE_NAME .env | cut -d '=' -f2)
+
+echo "☢ Executing TypeORM command ${@} with N args ${#}"
+echo "============================"
+docker-compose exec $CONTAINER_ID npm run typeorm:subscriber ${@} 
